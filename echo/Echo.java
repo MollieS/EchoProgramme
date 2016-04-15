e0191e84c7c36ef7f6a91f8c4663c427de4ea28c
Mollie Stephenson <molliestephenson@Mollies-MBP.default>
2016-04-15 11:54:48 +0100

    test for prompt output using console

--- echo/Echo.java
+++ echo/Echo.java
@@ -1,21 +1,4 @@
 package echo;
 
-import java.util.Scanner;
-
 public class Echo {
-
-  public static void main(String[] args) {
-    askForInput();
-    echo();
-  }
-
-  public static void askForInput() {
-    System.out.println(">");
-  }
-
-  public static void echo() {
-    Scanner scanner = new Scanner(System.in);
-    String output = scanner.next();
-    System.out.println(output);
-  }
 }
--- echo/EchoTest.java
+++ echo/EchoTest.java
@@ -1,42 +1,17 @@
 package echo;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
-import java.io.*;
 
 import echo.Echo;
 
 public class EchoTest {
-
-  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-  private final ByteArrayInputStream inContent = new ByteArrayInputStream("Hello".getBytes());
-
-  @Before
-  public void setUp() {
-    System.setOut(new PrintStream(outContent));
-  }
-
-  @After
-  public void reset() {
-    System.setOut(null);
-  }
-
+  
   @Test
-  public void asksForInput() {
-    Echo echo = new Echo();
-    echo.askForInput();
-    String output = new String(outContent.toByteArray());
-    assertTrue(output.contains(">"));
-  }
-
-  @Test
-  public void returnsInput() {
-    System.setIn(inContent);
-    Echo echo = new Echo();
-    echo.echo();
-    String output = new String(outContent.toByteArray());
-    assertTrue(output.contains("Hello"));
+  public void promptsUserInput() {
+    TestConsole testConsole = new TestConsole();
+    Echo echo = new Echo(testConsole);
+    echo.promptInput();
+    assertEquals(">", testConsole.output());
   }
 }
